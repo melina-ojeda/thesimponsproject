8b68339f8c152575731bbaa11e9d3d714ea462e5
Melina Ojeda <melina.ojeda@istea.com.ar>
2026-06-04 00:31:14 -0300

    update language to EN + fetch and render episodes

---
+++ app.js
@@ -0,0 +1,44 @@
+let page = 1;
+let temporadaActual = '';
+let querySearch = '';
+
+async function fetchEpisodios() {
+    const container = document.getElementById('grilla-episodios');
+    container.innerHTML = '';
+
+    let url = `https://thesimpsonsapi.com/api/episodes?page=${page}`;
+    if (temporadaActual) url += `&season=${temporadaActual}`;
+     console.log('URL:', url);
+    
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // búsqueda 
+    const filtrados = data.results.filter(ep =>
+    ep.name.toLowerCase().includes(querySearch.toLowerCase())
+    );
+
+    renderEpisodios(filtrados);
+    renderPaginacion(data);
+}
+
+function renderEpisodios(episodios) {
+    const container = document.getElementById('grilla-episodios');
+    episodios.forEach(ep => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+            <img src="https://cdn.thesimpsonsapi.com/200/${ep.image_path}" alt="${ep.name}">
+            <h3>${ep.name}</h3>
+            <p>Description: ${ep.synopsis}</p>
+            <p>Airdate: ${ep.airdate}</p>
+            <p>Season: ${ep.season}</p>
+            <p>Episode: ${ep.episode_number}</p>
+            <button onclick="guardarFav(${ep.id})">Save as Favorite</button>
+        `;
+        container.appendChild(card);
+    });
+}
+
+
+fetchEpisodios();
